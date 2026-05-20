1a6ae9a9f65e25ebb6b9ecfc072d594b7c1cddfa
Emanuel <emanuearaujocaceres@gmail.com>
2026-05-19 21:34:34 -0300

    fix: corrige caminho do layout.css para absoluto

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useEffect, useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
-import '../styles/layout.css'
+import '/src/styles/layout.css'  // ← CAMINHO ABSOLUTO
 import {
   BarChart3,
   Clock3,
